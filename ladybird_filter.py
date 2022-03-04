fe90238d254f3fa291d7c7132fcd8d7dc123c124
jaconfer <97074340+jaconfer@users.noreply.github.com>
2022-03-04 01:56:39 -0800

    Update ladybird_filter.py

--- ladybird_filter.py
+++ ladybird_filter.py
@@ -6,8 +6,9 @@ lb_list = re.split('\.\n|\?\n|!\n', lb_script)
 lb_utters = []
 for item in lb_list:
     line = item.replace('\n', '')
-    if re.search('\slike,\s', line):
-        lb_utters.append(line)
+    if re.search('\slike,\s|Like,\s', line):
+        line2 = re.sub('\slike,\s|Like,\s', ' LIKE, ---$--- ', line)
+        lb_utters.append(line2)
 with open('ladybird_likes.txt', 'w') as lbl:
     for item in lb_utters:
         lbl.write('\n>'+item)
