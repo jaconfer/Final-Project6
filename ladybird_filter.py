3c4b29e0b4534c5db34b34aae7e36eaf5fde9ffa
jaconfer <97074340+jaconfer@users.noreply.github.com>
2022-03-04 01:08:27 -0800

    Add files via upload

---
+++ ladybird_filter.py
@@ -0,0 +1,14 @@
+import re
+
+lb_script = open('ladybird.txt', 'r').read()
+lb_list = re.split('\.\n|\?\n|!\n', lb_script)
+
+lb_utters = []
+for item in lb_list:
+    line = item.replace('\n', '')
+    if re.search('\slike,\s', line):
+        lb_utters.append(line)
+with open('ladybird_likes.txt', 'w') as lbl:
+    for item in lb_utters:
+        lbl.write('\n>'+item)
+
