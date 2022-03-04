c7ecb556b9b35f9035c1d89f715aea4ba4fca3f6
jaconfer <97074340+jaconfer@users.noreply.github.com>
2022-03-04 01:53:52 -0800

    Update clueless_filter.py

--- clueless_filter.py
+++ clueless_filter.py
@@ -6,9 +6,9 @@ cl_list = re.split('\.\n|\?\n|!\n', cl_script)
 cl_utters = []
 for item in cl_list:
     line = item.replace('\n', '')
-    if re.search('\slike,\s', line):
-        cl_utters.append(line)
+    if re.search('\slike,\s|Like,\s', line):
+        line2 = re.sub('\slike,\s|Like,\s', ' LIKE, ---$--- ', line)
+        cl_utters.append(line2)
 with open('clueless_likes.txt', 'w') as cll:
     for item in cl_utters:
         cll.write('\n>'+item)
-
